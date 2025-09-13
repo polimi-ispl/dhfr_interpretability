a771924cacb3fcfabcf5d93c727c4ddf5ed03321
Edoardo <edoardodaniele.cannas@polimi.it>
2025-09-13 19:01:51 +0200

    network definition

---
+++ isplutils/network.py
@@ -0,0 +1,54 @@
+"""
+Network definition file
+
+All the networks deployed in the detector are defined in this file
+
+Authors:
+Edoardo Daniele Cannas - edoardodaniele.cannas@polimi.it
+Sara Mandelli - sara.mandelli@polimi.it
+Paolo Bestagini - paolo.bestagini@polimi.it
+Stefano Tubaro - stefano.tubaro@polimi.it
+"""
+
+# --- Libraries import
+import os
+import numpy as np
+import tensorflow as tf
+from keras.models import Model
+from keras.layers import Input, Conv2D, BatchNormalization, Activation, Lambda, Subtract
+
+
+# --- Network creator helpers
+def DnCNN(depth=17, filters=64, image_channels=1, use_bnorm=True, model_path=None):
+
+    layer_count = 0
+    inpt = Input(shape=(None, None, image_channels), name='input' + str(layer_count))
+    # 1st layer, Conv+relu
+    layer_count += 1
+    x = Conv2D(filters=filters, kernel_size=(3, 3), strides=(1, 1), kernel_initializer='Orthogonal', padding='same',
+               name='conv' + str(layer_count))(inpt)
+    layer_count += 1
+    x = Activation('relu', name='relu' + str(layer_count))(x)
+    # depth-2 layers, Conv+BN+relu
+    for i in range(depth - 2):
+        layer_count += 1
+        x = Conv2D(filters=filters, kernel_size=(3, 3), strides=(1, 1), kernel_initializer='Orthogonal',
+                   padding='same', use_bias=False, name='conv' + str(layer_count))(x)
+        if use_bnorm:
+            layer_count += 1
+            x = BatchNormalization(axis=-1, momentum=0.9, epsilon=0.0001, name='bn' + str(layer_count))(x)
+        layer_count += 1
+        x = Activation('relu', name='relu' + str(layer_count))(x)
+        # last layer, Conv
+    layer_count += 1
+    x = Conv2D(filters=image_channels, kernel_size=(3, 3), strides=(1, 1), kernel_initializer='Orthogonal',
+               padding='same', use_bias=False, name='conv' + str(layer_count))(x)
+    layer_count += 1
+    model = Model(inputs=inpt, outputs=x)
+
+    if model_path is not None:
+        final_weights_path = os.path.join(model_path, 'model_weights.h5')
+        model.load_weights(final_weights_path)
+        print('loaded ', final_weights_path)
+
+    return model
